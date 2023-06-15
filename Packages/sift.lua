deb16016d7a42fdbea93818441e63857e607dc5c
strawbberrys <strawbberrys@protonmail.com>
2023-06-15 19:40:11 -0400

    Create map generation

---
+++ Packages/sift.lua
@@ -0,0 +1,1 @@
+return require(script.Parent._Index["csqrl_sift@0.0.6"]["sift"])
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,22 @@
-print("Hello world, from server!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local Match = require(ServerScriptService.server.Match)
+
+local match = Match.new({
+    contestants = {},
+    controller = game:GetService("Players"):GetPlayers()[1],
+    duration = 15,
+    rounds = 5,
+    map = ReplicatedStorage.maps.bedroom,
+    mapConfig = {
+        boxConfig = {
+            perRow = 3,
+            perColumn = 3,
+            layers = 6,
+            template = Instance.new("Model")
+        }
+    },
+})
+
+match:start()
